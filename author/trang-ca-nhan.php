ec919bc94793844c54b7a6e1fb15f88873f060b3
Sơn Lê <son.le@cloudgo.vn>
2025-12-10 22:54:52 +0700

    [Page] Add user info page

--- author/trang-ca-nhan.php
+++ author/trang-ca-nhan.php
@@ -1,119 +1,137 @@
 <?php
-	if (!defined('MovieAnime')) die("You are illegally infiltrating our website");
-	if (!$_author_cookie) die(header("location:/login"));
+if (!defined('MovieAnime')) die("You are illegally infiltrating our website");
 
-	$userId = $value[2];
-	$iconCategory = categoryStore();
-	$listItemStore = listItemStore();
-	$listItemOwned = listUserItemOwner($userId, false);
-	$currentUser = GetDataArr('user', "id = $userId");
+ob_start();
 
-	if (!empty($currentUser)) {
-		$currentUser['khung-vien'] = getIconStoreActive($userId, 'khung-vien');
-		$currentUser['background'] = getIconStoreActive($userId, 'background');
+$userId = $value[2];
+$iconCategory = categoryStore();
+$listItemStore = listItemStore();
+$listItemOwned = listUserItemOwner($userId, false);
+$currentUser = GetDataArr('user', "id = $userId");
+
+if (!empty($currentUser)) {
+	$currentUser['khung-vien'] = getIconStoreActive($userId, 'khung-vien');
+	$currentUser['background'] = getIconStoreActive($userId, 'background');
+}
+
+$totalFollow = getTotalFollow($userId);
+
+$title = "Trang cá nhân - {$currentUser['nickname']} - {$cf['title']}";
+$description = "Xem trang cá nhân của {$currentUser['nickname']}";
+
+?>
+<style>
+	.page-title {
+		font-size: 17px;
 	}
 
-	$totalFollow = getTotalFollow($userId);
-?>
-<!DOCTYPE html>
-<html lang="vi">
+	.page-title::after {
+		content: none;
+	}
 
-<head>
-	<?php
-	$title = "Trang cá nhân - {$cf['title']}";
-	require_once(ROOT_DIR . '/view/head.php');
-	?>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" />
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.min.js"></script>
-	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script> -->
-	<script type="text/javascript" src="/themes/js_ob/croppie.js?v=1.7.4"></script>
-	<link href="/themes/styles/croppie.css?v=1.4.0" rel="stylesheet" />
-	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/autonumeric/4.10.0/autoNumeric.min.js"></script> -->
-	<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-	<link href="/themes/styles/trang-ca-nhan.css?v=<?= time() ?>" rel="stylesheet" />
-</head>
+	.btn-dark,
+	.btn-dark:hover,
+	.btn-dark:focus {
+		background-color: #212529;
+		color: #fff;
+	}
 
-<body class="scroll-bar">
-	<div id="fb-root"></div>
-	<div id="ah_wrapper">
-		<?php require_once(ROOT_DIR . '/view/header.php'); ?>
-		<div class="ah_content">
-			<div class="page-title fs-17 fw-700 padding-0-20 color-gray inline-flex height-40 flex-hozi-center bg-black border-l-t">Trang cá nhân</div>
-			<div class="profile">
-				<?php if (!empty($currentUser)): ?>
-				<div class="navigation">
-					<!-- Avatar -->
-					<div class="avatar" style="<?= $currentUser['background'] ? 'background-image: url(' . $currentUser['background'] . ');' : '' ?>">
-						<div class="img">
-							<img src="<?= $currentUser['avatar'] ?>" />
-							<img src="<?= $currentUser['khung-vien'] ?>" alt="" class="avatar-frame">
-						</div>
-						<div class="profile-info">
-							<h4><?= $currentUser['nickname'] ?></h4>
-							<h6 style="color: <?= LevelColor($currentUser['level']) ?>">Cấp <?= $currentUser['level'] ?></h6>
-							<?php if (isFollowed($userId)): ?>
-								<button class="btn btn-danger border-white btn-follow" type="button"><i class="fa-solid fa-user"></i>Đang theo dõi (<?= $totalFollow ?>)</button>
-							<?php else: ?>
-								<button class="btn btn-dark border-white btn-follow" type="button"><i class="fa-solid fa-user"></i>Theo dõi (<?= $totalFollow ?>)</button>
-							<?php endif; ?>
-						</div>
+	.nav-tabs .nav-link.active {
+		background-color: #fff;
+	}
+</style>
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" /> -->
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.min.js"></script> -->
+<script type="text/javascript" src="/themes/js_ob/croppie.js?v=1.7.4"></script>
+<link href="/themes/styles/croppie.css?v=1.4.0" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+<link href="/themes/styles/trang-ca-nhan.css?v=<?= time() ?>" rel="stylesheet" />
+<input type="hidden" id="view" value="movie-bookmark">
+<input type="hidden" id="limit" value="<?= $limit ?>">
+
+<div class="row container" id="wrapper">
+	<main id="main-content" class="col-xs-12 col-sm-12 col-md-12">
+		<div class="page-title fs-17 fw-700 padding-0-20 color-gray inline-flex height-40 flex-hozi-center bg-black border-l-t">Trang cá nhân</div>
+		<div class="profile">
+			<?php if (!empty($currentUser)): ?>
+			<div class="navigation">
+				<!-- Avatar -->
+				<div class="avatar" style="<?= $currentUser['background'] ? 'background-image: url(' . $currentUser['background'] . ');' : '' ?>">
+					<div class="img">
+						<img src="<?= $currentUser['avatar'] ?>" />
+						<img src="<?= $currentUser['khung-vien'] ?>" alt="" class="avatar-frame">
+					</div>
+					<div class="profile-info">
+						<h4><?= $currentUser['nickname'] ?></h4>
+						<h6 style="color: <?= LevelColor($currentUser['level']) ?>">Cấp <?= $currentUser['level'] ?></h6>
+						<?php if (isFollowed($userId)): ?>
+							<button class="btn btn-danger border-white btn-follow" type="button"><i class="fa-solid fa-user"></i>Đang theo dõi (<?= $totalFollow ?>)</button>
+						<?php else: ?>
+							<button class="btn btn-dark border-white btn-follow" type="button"><i class="fa-solid fa-user"></i>Theo dõi (<?= $totalFollow ?>)</button>
+						<?php endif; ?>
 					</div>
 				</div>
-				<div class="tabs">
-					<ul class="nav nav-tabs" role="tablist">
-						<li class="nav-item menu-item hvr-sweep-to-right">
-							<a class="nav-link active" href="#tab-movie" data-bs-toggle="tab">Phim theo dõi</a>
-						</li>
-						<li class="nav-item menu-item hvr-sweep-to-right">
-							<a class="nav-link" href="#tab-icon" data-bs-toggle="tab">Vật phẩm sở hữu</a>
-						</li>
-					</ul>
-					<div class="tab-content">
-						<div class="tab-pane fade show active" id="tab-movie">
-							<div class="tab-body mb-4 movie-follow">
+			</div>
+			<div class="tabs">
+				<ul class="nav nav-tabs" role="tablist">
+					<li class="nav-item menu-item hvr-sweep-to-right">
+						<a class="nav-link active" href="#tab-movie" data-bs-toggle="tab">Phim theo dõi</a>
+					</li>
+					<li class="nav-item menu-item hvr-sweep-to-right">
+						<a class="nav-link" href="#tab-icon" data-bs-toggle="tab">Vật phẩm sở hữu</a>
+					</li>
+				</ul>
+				<div class="tab-content">
+					<div class="tab-pane show active" id="tab-movie">
+						<div class="tab-body mb-4 movie-follow">
 
-							</div>
 						</div>
-						<div class="tab-pane fade" id="tab-icon">
-							<div class="tab-body mb-4">
-								<div class="icons">
-									<div class="grid">
-										<?php foreach ($listItemOwned as $cat => $list): ?>
-											<?php if (!empty($list)): ?>
-											<div class="icon-cat <?= $cat ?>">
-												<p><?= $iconCategory[$cat] ?></p>
-												<div class="list">
-													<?php foreach ($list as $iconId): ?>
-														<?php
-															$tooltip = '<img src="'.$listItemStore[$cat][$iconId]['image'].'" /><span class="icon-name">'.$listItemStore[$cat][$iconId]['name'].'</span>';
-														?>
-														<a href="javascript:void(0)" data-tooltip-icon='<?= $tooltip ?>'><img src="<?= $listItemStore[$cat][$iconId]['image'] ?>" alt="<?= $listItemStore[$cat][$iconId]['name'] ?>"></a>
-													<?php endforeach;?>
-												</div>
+					</div>
+					<div class="tab-pane" id="tab-icon">
+						<div class="tab-body mb-4">
+							<div class="icons">
+								<div class="grid">
+									<?php foreach ($listItemOwned as $cat => $list): ?>
+										<?php if (!empty($list)): ?>
+										<div class="icon-cat <?= $cat ?>">
+											<p><?= $iconCategory[$cat] ?></p>
+											<div class="list">
+												<?php foreach ($list as $iconId): ?>
+													<?php
+														$tooltip = '<img src="'.$listItemStore[$cat][$iconId]['image'].'" /><span class="icon-name">'.$listItemStore[$cat][$iconId]['name'].'</span>';
+													?>
+													<a href="javascript:void(0)" data-tooltip-icon='<?= $tooltip ?>'><img src="<?= $listItemStore[$cat][$iconId]['image'] ?>" alt="<?= $listItemStore[$cat][$iconId]['name'] ?>"></a>
+												<?php endforeach;?>
 											</div>
-											<?php endif?>
-										<?php endforeach;?>
-									</div>
+										</div>
+										<?php endif?>
+									<?php endforeach;?>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				<?php else: ?>
-					<p class="mb-0 mt-3 text-center">Không có dữ liệu</p>
-				<?php endif; ?>
 			</div>
+			<?php else: ?>
+				<p class="mb-0 mt-3 text-center">Không có dữ liệu</p>
+			<?php endif; ?>
 		</div>
-	</div>
-</body>
-
+	</main>
+</div>
 <script type="text/javascript">
 	$('body').on('click', '[data-bs-toggle="tab"]', function(e) {
-		$('[data-bs-toggle="tab"]').css('display', 'block');
+		const element = $(this);
+		const tab = element.attr('href');
+
+		element.closest('.nav-tabs').find('.nav-item .nav-link').removeClass('active');
+		element.addClass('active');
+		$('.tab-content .tab-pane').removeClass('show active');
+		$(tab).addClass('show active');
+		e.preventDefault();
 	});
 
 	$(document).ready(function() {
@@ -180,5 +198,8 @@
 
 	// End movie follow
 </script>
+<?php
+$content = ob_get_clean();
 
-</html>
+// Include layout
+require_once(ROOT_DIR . '/view/layout.php');
